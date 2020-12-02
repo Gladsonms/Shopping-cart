8e469b59110fcd102024bb42433679e11a960892
gladsonsaji <gladsonsaji4@gmail.com>
2020-12-02 20:20:05 +0530

    database connection

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 var express = require('express');
+const {render}=require('../app');
+const productsHelpers = require('../helpers/products-helpers');
 var router = express.Router();
-
+var productHelper=require('../helpers/products-helpers')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
@@ -36,7 +38,18 @@ router.get('/add-product',function(req,res){
 res.render('admin/add-product')
 })
 router.post('/add-product',(req,res)=>{
-console.log(req.body);
-console.log(req.files.image);
+
+productsHelpers.addProduct(req.body,(id)=>{
+  let image=req.files.Image
+  console.log(id);
+  image.mv('./public/product-images/'+id+'.jpg',(err)=>{
+    if(!err){
+      res.render("admin/add-product")
+    }else{
+      console.log(err);
+    }
+  })
+  
+})
 })
 module.exports = router;
